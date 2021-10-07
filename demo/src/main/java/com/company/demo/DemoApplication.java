11e655936163a7e1f121ee208b9789ce17576dab
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2021-10-07 14:03:49 +0100

    tweaks

--- demo/src/main/java/com/company/demo/DemoApplication.java
+++ demo/src/main/java/com/company/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.company.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @RestController
