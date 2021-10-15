ace18ee450862a18ee2316683bdc2d6759f61d03
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2021-10-15 14:42:29 +0100

    Added bean definitions

--- REST-Service/src/main/java/com/example/RESTService/RestServiceApplication.java
+++ REST-Service/src/main/java/com/example/RESTService/RestServiceApplication.java
@@ -2,12 +2,19 @@ package com.example.RESTService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
+		ApplicationContext ac = SpringApplication.run(RestServiceApplication.class, args);
+		int i = 1;
+		for (String bean : ac.getBeanDefinitionNames()) {
+			System.out.println(i + ". " + bean);
+			++i;
+			System.out.println(ac.isSingleton(bean) ? "Yes" : "No");
+		}
 	}
 
 }
