53450094f0a4c723a50f2163b0f1394e2997b90e
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2021-10-15 14:07:56 +0100

    Added bean definitions

--- FormHandling/src/main/java/com/example/FormHandling/FormHandlingApplication.java
+++ FormHandling/src/main/java/com/example/FormHandling/FormHandlingApplication.java
@@ -2,12 +2,19 @@ package com.example.FormHandling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FormHandlingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FormHandlingApplication.class, args);
+		ApplicationContext ac = SpringApplication.run(FormHandlingApplication.class, args);
+		int i = 1;
+		for (String bean : ac.getBeanDefinitionNames()) {
+			System.out.println(i + ". " + bean);
+			++i;
+			System.out.println(ac.isSingleton(bean) ? "Yes" : "No");
+		}
 	}
 
 }
